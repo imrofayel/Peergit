a894dcc8dc698e8c99f80141ba0c3ed906044372
rofayels <adamrofayel@gmail.com>
2025-02-13 15:24:59 +0500

    Update site title and Open Graph/Twitter metadata for improved branding

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -23,19 +23,19 @@ export default defineNuxtConfig({
       htmlAttrs: {
         lang: 'en'
       },
-      title: 'PeerGit',
+      title: 'Peergit',
       meta: [
         { charset: 'utf-8' },
         { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         { name: 'format-detection', content: 'telephone=no' },
         { name: 'description', content: "Discover a fun, playful, and engaging analysis of a GitHub user's profile!" },
         { name: 'keywords', content: 'GitHub profile analysis, GitHub user insights, developer journey, casual coding analysis, fun GitHub profile review, coding humor, repo insights, developer personality, GitHub tips, GitHub profile critique, developer evolution, top languages, most starred repo, beginner coding tips, fun coding reviews, coding profile roast, developer improvement tips' },
-        { property: 'og:title', content: 'PeerGit - a GitHub Peer for you!' },
+        { property: 'og:title', content: 'Peergit - a GitHub Peer for you!' },
         { property: 'og:description', content: "Discover a fun, playful, and engaging analysis of a GitHub user's profile!" },
         { property: 'og:type', content: 'website' },
         { property: 'og:image', content: '/og-image.jpg' },
         { name: 'twitter:card', content: 'summary_large_image' },
-        { name: 'twitter:title', content: 'PeerGit - a GitHub Peer for you!' },
+        { name: 'twitter:title', content: 'Peergit - a GitHub Peer for you!' },
         { name: 'twitter:description', content: "Discover a fun, playful, and engaging analysis of a GitHub user's profile!" },
         { name: 'twitter:image', content: '/og-image.jpg' }
       ],
