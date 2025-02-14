4c298c29bad99787e6db51ee21c8b165fcea2816
rofayels <adamrofayel@gmail.com>
2025-02-14 13:45:54 +0500

    Add Product Hunt badge to Hero component for promotion

--- components/site/Hero.vue
+++ components/site/Hero.vue
@@ -2,6 +2,8 @@
 
   <div class="sm:max-w-2xl mx-auto px-4 sm:px-0">
 
+    <a href="https://www.producthunt.com/posts/peergit?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-peergit" target="_blank" class="w-full items-center flex text-center justify-center mt-6"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=879347&theme=light&t=1739522652548" alt="Peergit - &#0032;Get&#0032;a&#0032;Fun&#0044;&#0032;Honest&#0032;Take&#0032;on&#0032;Your&#0032;GitHub&#0032;Profile&#0033; | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" /></a>
+
   <div class="w-full flex flex-col items-center justify-center">
 
     <h1 class="instrument-italic pt-12 pb-4 text-[55px] text-center drop-shadow-xs leading-[1]">a GitHub <u class="instrument-italic decoration-3 decoration-wavy underline-offset-4">Peer</u> for you.</h1>
