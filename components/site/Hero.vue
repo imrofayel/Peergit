0bd481ea845bfe588f734a1885b96719c7ca02a7
rofayels <adamrofayel@gmail.com>
2025-02-17 14:16:26 +0500

    Refactor Hero component to replace Product Hunt badge with Peerlist link and adjust button layout for better structure

--- components/site/Hero.vue
+++ components/site/Hero.vue
@@ -2,7 +2,12 @@
 
   <div class="sm:max-w-2xl mx-auto px-4 sm:px-0">
 
-    <a href="https://www.producthunt.com/posts/peergit?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-peergit" target="_blank" class="w-full items-center flex text-center justify-center mt-6"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=879347&theme=light&t=1739522652548" alt="Peergit - &#0032;Get&#0032;a&#0032;Fun&#0044;&#0032;Honest&#0032;Take&#0032;on&#0032;Your&#0032;GitHub&#0032;Profile&#0033; | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" /></a>
+<div class="flex">
+  
+  <a href="https://peerlist.io/imrofayel/project/peergit" target="_blank" class="w-full items-center flex text-center justify-center mt-6"><img src="/assets/Launch_SVG_Light.svg" alt="Peergit - &#0032;Get&#0032;a&#0032;Fun&#0044;&#0032;Honest&#0032;Take&#0032;on&#0032;Your&#0032;GitHub&#0032;Profile&#0033; | Peergit" style="width: 250px; height: 65px;" width="200" height="54" /></a>
+
+  <!-- <a href="https://www.producthunt.com/posts/peergit?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-peergit" target="_blank" class="w-full items-center flex text-center justify-center mt-6"><img src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=879347&theme=light&t=1739522652548" alt="Peergit - &#0032;Get&#0032;a&#0032;Fun&#0044;&#0032;Honest&#0032;Take&#0032;on&#0032;Your&#0032;GitHub&#0032;Profile&#0033; | Product Hunt" style="width: 250px; height: 54px;" width="250" height="54" /></a> -->
+</div>
 
   <div class="w-full flex flex-col items-center justify-center">
 
